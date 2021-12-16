949a491a284d27e12c021d24da56e511d04629d1
黎聪 <licong23@xdf.cn>
2021-12-16 11:25:59 +0800

    feat: 添加连接池

--- src/sequelize/seq.js
+++ src/sequelize/seq.js
@@ -1,10 +1,19 @@
 // sequelize演示
 const Sequelize = require('sequelize')
 
-const seq = new Sequelize('case2-microblog', 'root', '123456', {
+const conf = {
 	host: 'localhost',
 	dialect: 'mysql'
-})
+}
+
+// 线上环境使用连接池
+// conf.pool = {
+// 	max: 5,	// 连接池中最大的连接数量
+// 	min: 0,	// 最小数量
+// 	idle: 10000, // 如果一个连接池 10s 之内没有被使用，则释放
+// }
+
+const seq = new Sequelize('case2-microblog', 'root', '123456', conf)
 
 // 测试连接
 // seq.authenticate()
