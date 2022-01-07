67bb923a2cf39851f1eb01e063844f081010a966
黎聪 <licong23@xdf.cn>
2022-01-07 09:52:55 +0800

    feat: 修改密码和退出登录

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,12 @@
 /**
  * user controller
  */
-const { getUserInfo, createUser, deleteUser, updateUser } = require('../services/user');
+const {
+  getUserInfo,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require('../services/user');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const {
   registerUserNameNotExistInfo,
@@ -139,6 +144,39 @@ async function changeInfo(ctx, { nickName, city, picture }) {
   return new ErrorModel(changeInfoFailInfo);
 }
 
+/**
+ * 修改密码
+ * @param {string} userName 用户名
+ * @param {string} password 当前密码
+ * @param {string} newPassword 新密码
+ */
+async function changePassword(userName, password, newPassword) {
+  const result = await updateUser(
+    {
+      newPassword: doCrypto(newPassword),
+    },
+    {
+      userName,
+      password: doCrypto(password),
+    }
+  );
+  if (result) {
+    // 成功
+    return new SuccessModel();
+  }
+  // 失败
+  return new ErrorModel(changePasswordFailInfo);
+}
+
+/**
+ * 退出登录
+ * @param {Object} ctx ctx
+ */
+async function logout(ctx) {
+  delete ctx.session.userInfo;
+  return new SuccessModel();
+}
+
 module.exports = {
   isExist,
   register,
@@ -146,4 +184,6 @@ module.exports = {
   getLoginInfo,
   deleteCurUser,
   changeInfo,
+  changePassword,
+  logout,
 };
