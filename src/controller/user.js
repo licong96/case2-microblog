8a885fbda99a9508761bfbf9b51be19abc3a3661
黎聪 <licong23@xdf.cn>
2022-01-07 09:33:55 +0800

    feat: 修改个人信息

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,7 @@
 /**
  * user controller
  */
-const { getUserInfo, createUser, deleteUser } = require('../services/user');
+const { getUserInfo, createUser, deleteUser, updateUser } = require('../services/user');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const {
   registerUserNameNotExistInfo,
@@ -9,6 +9,7 @@ const {
   registerFailInfo,
   loginFailInfo,
   deleteUserFailInfo,
+  changeInfoFailInfo,
 } = require('../model/ErrorInfo');
 const doCrypto = require('../utils/cryp');
 
@@ -103,10 +104,46 @@ async function deleteCurUser(userName) {
   return new ErrorModel(deleteUserFailInfo);
 }
 
+/**
+ * 修改个人信息
+ * @param {Object} ctx ctx
+ * @param {string} nickName 昵称
+ * @param {string} city 城市
+ * @param {string} picture 头像
+ */
+async function changeInfo(ctx, { nickName, city, picture }) {
+  const { userName } = ctx.session.userInfo;
+  if (!nickName) {
+    nickName = userName;
+  }
+
+  const result = await updateUser(
+    {
+      newNickName: nickName,
+      newCity: city,
+      newPicture: picture,
+    },
+    { userName }
+  );
+  if (result) {
+    // 执行成功
+    Object.assign(ctx.session.userInfo, {
+      nickName,
+      city,
+      picture,
+    });
+    // 返回
+    return new SuccessModel();
+  }
+  // 失败
+  return new ErrorModel(changeInfoFailInfo);
+}
+
 module.exports = {
   isExist,
   register,
   login,
   getLoginInfo,
   deleteCurUser,
+  changeInfo,
 };
