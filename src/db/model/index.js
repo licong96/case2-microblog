77c6055dc5230f478dc21e7847c8c160faf13132
黎聪 <licong23@xdf.cn>
2022-01-10 15:49:29 +0800

    feat: 添加数据模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -4,13 +4,22 @@
 
 const User = require('./User');
 const Blog = require('./Blog');
+const UserRelation = require('./UserRelation');
 
 // 可以通过Blog表查到User信息
 Blog.belongsTo(User, {
   foreignKey: 'userId',
 });
 
+UserRelation.belongsTo(User, {
+  foreignKey: 'followerId',
+});
+User.hasMany(UserRelation, {
+  foreignKey: 'userId',
+});
+
 module.exports = {
   User,
   Blog,
+  UserRelation,
 };
