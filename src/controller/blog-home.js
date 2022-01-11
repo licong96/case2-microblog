482b7642b14c113b1bbc7a9b567ad32216021767
黎聪 <licong23@xdf.cn>
2022-01-11 14:03:05 +0800

    feat: 首页获取关注人微博

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -3,10 +3,10 @@
  */
 
 const xss = require('xss');
-const { createBlog } = require('../services/blog');
+const { createBlog, getFollowersBlogList } = require('../services/blog');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const { createBlogFailInfo } = require('../model/ErrorInfo');
-// const { PAGE_SIZE, REG_FOR_AT_WHO } = require('../conf/constant');
+const { PAGE_SIZE, REG_FOR_AT_WHO } = require('../conf/constant');
 // const { getUserInfo } = require('../services/user');
 // const { createAtRelation } = require('../services/at-relation');
 
@@ -24,7 +24,7 @@ async function create({ userId, content, image }) {
     });
     // 返回
     return new SuccessModel({
-      id: blog.id
+      id: blog.id,
     });
   } catch (ex) {
     console.error(ex.message, ex.stack);
@@ -32,6 +32,30 @@ async function create({ userId, content, image }) {
   }
 }
 
+/**
+ * 获取首页微博列表
+ * @param {number} userId userId
+ * @param {number} pageIndex page index
+ */
+async function getHomeBlogList(userId, pageIndex = 0) {
+  const result = await getFollowersBlogList({
+    userId,
+    pageIndex,
+    pageSize: PAGE_SIZE,
+  });
+  const { count, blogList } = result;
+
+  // 返回
+  return new SuccessModel({
+    isEmpty: blogList.length === 0,
+    blogList,
+    pageSize: PAGE_SIZE,
+    pageIndex,
+    count,
+  });
+}
+
 module.exports = {
   create,
+  getHomeBlogList,
 };
--- src/services/blog.js
+++ src/services/blog.js
@@ -1,7 +1,7 @@
 /**
  * @description blog services
  */
-const { Blog, User } = require('../db/model/index');
+const { Blog, User, UserRelation } = require('../db/model/index');
 const { formatUser, formatBlog } = require('./_format');
 
 /**
@@ -60,7 +60,44 @@ async function getBlogListByUser({ userName, pageIndex = 0, pageSize = 10 }) {
   };
 }
 
+/**
+ * 获取关注着的微博列表（首页）
+ * @param {Object} param0 查询条件 { userId, pageIndex = 0, pageSize = 10 }
+ */
+async function getFollowersBlogList({ userId, pageIndex = 0, pageSize = 10 }) {
+  const result = await Blog.findAndCountAll({
+    limit: pageSize, // 每页多少条
+    offset: pageSize * pageIndex, // 跳过多少条
+    order: [['id', 'desc']],
+    include: [
+      {
+        model: User,
+        attributes: ['userName', 'nickName', 'picture'],
+      },
+      {
+        model: UserRelation,
+        attributes: ['userId', 'followerId'],
+        where: { userId },
+      },
+    ],
+  });
+
+  // 格式化数据
+  let blogList = result.rows.map((row) => row.dataValues);
+  blogList = formatBlog(blogList);
+  blogList = blogList.map((blogItem) => {
+    blogItem.user = formatUser(blogItem.user.dataValues);
+    return blogItem;
+  });
+
+  return {
+    count: result.count,
+    blogList,
+  };
+}
+
 module.exports = {
   createBlog,
   getBlogListByUser,
+  getFollowersBlogList,
 };
